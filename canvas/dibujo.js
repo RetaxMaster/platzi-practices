764852e6246a87793e55c54144f6cf694ce8262b
RetaxMaster <retaxmaster@hotmail.com>
2020-09-28 21:45:22 -0500

    Mejoras en el codigo de canvas

--- canvas/dibujo.js
+++ canvas/dibujo.js
@@ -1,8 +1,15 @@
 var d = document.getElementById("dibujito");
 var lienzo = d.getContext("2d");
+var lineas = 30;
+var l = 0;
+var xi, yf;
+var colorcito = "#faa";
 
-dibujarLinea("pink", 10, 300, 220, 10);
-dibujarLinea("yellow", 300, 10, 10, 220);
+for(l = 0; l < lineas; l++)
+    dibujarLinea(colorcito, 10 * l, 0, 300, 10 * (l +1));
+
+dibujarLinea(colorcito, 299, 0, 299, 299);
+dibujarLinea(colorcito, 1, 1, 299, 1);
 
 function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal) {
 
