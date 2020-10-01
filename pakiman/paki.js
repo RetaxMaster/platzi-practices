774c3d6d182822003ab974107abb78d0f814a30f
RetaxMaster <retaxmaster@hotmail.com>
2020-10-01 16:08:34 -0500

    Sincronizacion del proyecto

--- pakiman/paki.js
+++ pakiman/paki.js
@@ -1,4 +1,4 @@
-var coleccion = [];
+/* var coleccion = [];
 
 coleccion.push(new Pakiman("Cauchin", 100, 30));
 coleccion.push(new Pakiman("Pokacho", 80, 50));
@@ -8,4 +8,26 @@ console.log(coleccion);
 
 for (var pakin of coleccion) {    
     pakin.mostrar();
-}
+} */
+
+var usuario = prompt("Cuatos pesas?");
+        var planeta = parseInt(prompt("Elige un planeta\n Escribe 1 para Marte y 2 para Jupiter"));
+        var peso = parseFloat(usuario);
+        var gravedadTierra = 9.8;
+        var gravedadMarte = 3.7;
+        var gravedadJupiter = 24.8;
+        //var peso = 65;
+        var pesoMarte;
+        var pesoJupiter;
+        pesoMarte = peso * gravedadMarte / gravedadTierra;
+        pesoJupiter = peso * gravedadJupiter /gravedadTierra;
+
+        if (planeta == 1){
+          document.write("Tu peso en Marte es de <strong>",parseInt(pesoMarte)," kg </strong>");
+        } else if (planeta == 2) {
+        
+        
+        document.write("Tu peso en Jupiter es de <strong>",parseInt(pesoJupiter)," Kg</strong>");
+        }else{
+          ducument.write("Tas mal");
+        }
